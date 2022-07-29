dc33ee2c041838869536812116ae34ca8db89798
dhruv50ae <linkdp2012@gmail.com>
2022-07-29 12:53:35 +0100

    Create foundation for final project

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import requests
 
 APIKEY = "f58a6a7a3b81aa9698d51ad854c43102"
+APIURL = "https://api.openweathermap.org/data/2.5/weather"
 
-MY_LAT = 51.510357
-MY_LONG = -0.116773
-
-APIURL = "https://api.openweathermap.org/data/3.0/onecall"
 PARAMS = {
-    "lat": MY_LAT, "lon": MY_LONG, "appid": APIKEY
+    "q": "Leicester,UK", "appid": APIKEY
 }
+
+willRain = False
 
 response = requests.get(url=APIURL, params=PARAMS)
 response.raise_for_status()
-print(response.json())
+weatherData = response.json()
+conditionCode = weatherData["weather"][0]["id"]
+
+if int(conditionCode) < 700:
+    willRain = True
+if willRain:
+    print("It will rain, bring an umbrella")
